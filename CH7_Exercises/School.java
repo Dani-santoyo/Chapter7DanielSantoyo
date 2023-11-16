01b4bb9dd6f9a919f8fac42fa1200adf1c3e3514
Dani-santoyo <142623407+Dani-santoyo@users.noreply.github.com>
2023-11-16 15:05:08 -0500

    Add files via upload

---
+++ CH7_Exercises/School.java
@@ -0,0 +1,27 @@
+package CH7_Exercises;
+import java.util.*;
+public class School {
+    //is this instance variable and constructor correct for the "School" class?
+    ArrayList<Student> studentList = new ArrayList<Student>();
+    public School(ArrayList studentList){
+        this.studentList = studentList;
+    }
+    public void addStudent(Student student){
+        studentList.add(student);
+    }
+    public void removeStudent(Student student){
+        studentList.remove(student);
+    }
+    //the findYoungestStudent method below: I am having trouble getting the integer value "age"
+    // from the first element of my array list "studentList"
+    public Student findYoungestStudent(ArrayList studentList){
+        Student youngest = studentList.get(0);
+        for(int i = 0; i < studentList.size(); i++){
+            if(studentList.get(i).getAge() > Student.getAge()){
+                youngest = (Student) studentList.get(i);
+            }
+        }
+        return Arrays.toString(youngest);
+    }
+
+}
